8ca2358f5c8c878aa2c8eb65ecc6cf9a8f84f09e
Hafsa600-dot <princehafsa156@gmail.com>
2024-11-18 21:47:39 +0500

    .

--- color.js
+++ color.js
@@ -1,12 +1,21 @@
 const buttons = document.querySelectorAll(".buttons");
 const body = document.querySelector("body");
 
-buttons.forEach(function (buttons) {
-  console.log(buttons);
-  buttons.addEventListener("click", function (e) {
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener("click", function (e) {
     console.log(e);
     console.log(e.target);
     if (e.target.id === "red") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "green") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellow") {
       body.style.backgroundColor = e.target.id;
     }
   });
