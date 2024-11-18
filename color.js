7e11b94e77ec7e8b40b935eafb53c483f823839c
Hafsa600-dot <princehafsa156@gmail.com>
2024-11-18 22:00:15 +0500

    .

--- color.js
+++ color.js
@@ -1,6 +1,5 @@
-const buttons = document.querySelectorAll(".buttons");
+const buttons = document.querySelectorAll(".button");
 const body = document.querySelector("body");
-
 buttons.forEach(function (button) {
   console.log(button);
   button.addEventListener("click", function (e) {
@@ -9,13 +8,13 @@ buttons.forEach(function (button) {
     if (e.target.id === "red") {
       body.style.backgroundColor = e.target.id;
     }
-    if (e.target.id === "green") {
-      body.style.backgroundColor = e.target.id;
-    }
     if (e.target.id === "blue") {
       body.style.backgroundColor = e.target.id;
     }
     if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "green") {
       body.style.backgroundColor = e.target.id;
     }
   });
