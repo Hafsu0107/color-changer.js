b8998e7df160d149cd47e999978b499547b94591
Hafsa600-dot <princehafsa156@gmail.com>
2024-11-19 17:57:23 +0500

    .

--- guess-the-numb.js
+++ guess-the-numb.js
@@ -1,1 +1,35 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+const submit = document.querySelector("#subt");
+const userInput = document.querySelector("#guessField");
+const guessSlot = document.querySelector(".guesses");
+const remaining = document.querySelector(".lastResult");
+const lowOhri = document.querySelector(".LowOhri");
+const startOver = document.querySelector(".resultParas");
 
+const p = document.createElement("p");
+
+let prevGuess = [];
+let numGuess = 1;
+let playGame = true;
+
+function validateGuess(guess) {
+  //
+}
+
+function checkGuess(guess) {
+  //
+}
+function displayGuess(guess) {
+  //
+}
+
+function displayMessage(meassage) {
+  //
+}
+
+function newGame() {
+  //
+}
+function endGame() {
+  //
+}
